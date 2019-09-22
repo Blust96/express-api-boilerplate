021da4edcea30e8a232cd70f2a8802b6218d9a4c
Quentin <q.bohain@gmail.com>
2019-09-22 19:44:33 +0200

    Adding patch and delete routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const passport = require('passport');
+
+const { Mandatories, checkBody } = require('../middlewares/bodyChecker');
 const { setAuthentication } = require('../middlewares/authentication');
 setAuthentication(passport);
 
@@ -13,5 +15,11 @@ usersRouter.get('/', passport.authenticate('jwt', { session: false }), controlle
 
 // Get by ID
 usersRouter.get('/:id', passport.authenticate('jwt', { session: false }), controller.getById);
+
+// Update by ID
+usersRouter.patch('/:id', passport.authenticate('jwt', { session: false }), controller.updateById);
+
+// Delete by ID
+usersRouter.delete('/:id', passport.authenticate('jwt', { session: false }), controller.deleteById);
 
 module.exports = usersRouter;
