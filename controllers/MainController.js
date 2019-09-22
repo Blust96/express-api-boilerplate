398997e4f511adeb5b2538b8fa351c76e41d0991
Quentin <q.bohain@gmail.com>
2019-09-22 23:03:12 +0200

    updateById and deleteById methods implementation

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -6,6 +6,8 @@ class MainController {
         this.model = model;
         this.getAll = this.getAll.bind(this);
         this.getById = this.getById.bind(this);
+        this.updateById = this.updateById.bind(this);
+        this.deleteById = this.deleteById.bind(this);
     }
 
     /**
@@ -42,6 +44,44 @@ class MainController {
             if(!item) sendErrorResponse(res, 404, 'Resource not found');
             // Send response
             else sendApiResponse(res, 200, 'Data successfully received', { item });
+        });
+
+    }
+
+    /**
+     * Update by ID
+     * 
+     * @param {Request} req 
+     * @param {Response} res 
+     */
+    async updateById(req, res) {
+
+        this.model.findOneAndUpdate({ _id: req.params.id }, req.body, { 'new': true }, (error, item) => {
+            // If error occurs
+            if(error) sendErrorResponse(res, 500, 'An error occured while updating resource', { error });
+            // If items not found
+            if(!item) sendErrorResponse(res, 404, 'Resource not found');
+            // Send response
+            else sendApiResponse(res, 200, 'Data successfully updated', { item });
+        });
+
+    }
+
+    /**
+     * Delete by ID
+     * 
+     * @param {Request} req 
+     * @param {Response} res 
+     */
+    async deleteById(req, res) {
+
+        this.model.findOneAndDelete({ _id: req.params.id }, (error, item) => {
+            // If error occurs
+            if(error) sendErrorResponse(res, 500, 'An error occured while deleting resource', { error });
+            // If items not found
+            if(!item) sendErrorResponse(res, 404, 'Resource not found');
+            // Send response
+            else sendApiResponse(res, 200, 'Data successfully deleted', { item });
         });
 
     }
