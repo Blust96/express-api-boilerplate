45be1091a09e71cfaea9dbd066416eb2ec5188c8
Quentin <q.bohain@gmail.com>
2019-09-24 17:11:58 +0200

    Removing protected router

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,12 @@ const users = require('./users');
 // Router definition
 const routes = Router();
 const apiRoutes = Router();
-const protectedRoutes = Router();
 
 // Main routes
 routes.use('/api', apiRoutes);
 
 // API routes
-apiRoutes.use('/protected', protectedRoutes);
 apiRoutes.use('/auth', auth);
-
-// Protected API routes
-protectedRoutes.use('/users', users);
+apiRoutes.use('/users', users);
 
 module.exports = { routes };
