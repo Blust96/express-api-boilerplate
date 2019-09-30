d93023b7379d0409f0ac28b35399acfcbe08881a
Quentin <q.bohain@gmail.com>
2019-09-30 16:49:04 +0200

    Filters integration

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const { Router } = require('express');
+const passport = require('passport');
 
-const { Filters, checkRequiredFields } = require('../middlewares/requestsChecker');
+const { checkRequiredFields } = require('../middlewares/requestsChecker');
+const { setAuthentication } = require('../middlewares/authentication');
+setAuthentication(passport);
+
+const authReqFilters = require('../services/filters').requests.auth;
 
 const AuthController = require('../controllers/AuthController');
 const controller = new AuthController();
@@ -12,13 +17,32 @@ const authRouter = Router();
  * @route POST api/auth/login
  * @access Public
  */
-authRouter.post('/login', checkRequiredFields(Filters.auth.login), controller.login);
+authRouter.post(
+    '/login', 
+    checkRequiredFields(authReqFilters.login), 
+    controller.login
+);
+
+/**
+ * @desc User login by token
+ * @route GET api/auth/login/token
+ * @access Private
+ */
+authRouter.get(
+    '/login/token', 
+    passport.authenticate('jwt', { session: false }), 
+    controller.tokenLogin
+);
 
 /**
  * @desc User register
  * @route POST api/auth/register
  * @access Public
  */
-authRouter.post('/register', checkRequiredFields(Filters.auth.register), controller.register);
+authRouter.post(
+    '/register', 
+    checkRequiredFields(authReqFilters.register), 
+    controller.register
+);
 
 module.exports = authRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 const passport = require('passport');
 
-const { Filters, checkRequiredFields, checkExtraFields } = require('../middlewares/requestsChecker');
+const { checkExtraFields } = require('../middlewares/requestsChecker');
 const { setAuthentication } = require('../middlewares/authentication');
 setAuthentication(passport);
+
+const usersReqFilters = require('../services/filters').requests.users;
+const usersResFilters = require('../services/filters').responses.users;
 
 const UsersController = require('../controllers/UsersController');
 const controller = new UsersController();
@@ -11,31 +14,45 @@ const controller = new UsersController();
 const usersRouter = Router();
 
 /**
- * @route GET api/protected/users
+ * @route GET api/users
  * @desc Get all users
  * @access Public
  */
-usersRouter.get('/', controller.getAll);
+usersRouter.get(
+    '/', 
+    (req, res) => controller.getAll(req, res, usersResFilters)
+);
 
 /**
- * @route GET api/protected/users/:id
+ * @route GET api/users/:id
  * @desc Get user by id
  * @access Public
  */
-usersRouter.get('/:id', controller.getById);
+usersRouter.get(
+    '/:id', 
+    (req, res) => controller.getById(req, res, usersResFilters)
+);
 
 /**
- * @route PUT api/protected/users/:id
+ * @route PUT api/users/:id
  * @desc Update user by id
  * @access Private
  */
-usersRouter.put('/:id', [passport.authenticate('jwt', { session: false }), checkExtraFields(Filters.users.update)], controller.updateById);
+usersRouter.put(
+    '/:id', 
+    [passport.authenticate('jwt', { session: false }), checkExtraFields(usersReqFilters.update)], 
+    (req, res) => controller.updateById(req, res, usersResFilters)
+);
 
 /**
- * @route DELETE api/protected/users/:id
+ * @route DELETE api/users/:id
  * @desc Delete user by id
  * @access Private
  */
-usersRouter.delete('/:id', passport.authenticate('jwt', { session: false }), controller.deleteById);
+usersRouter.delete(
+    '/:id', 
+    passport.authenticate('jwt', { session: false }), 
+    (req, res) => controller.deleteById(req, res, usersResFilters)
+);
 
 module.exports = usersRouter;
